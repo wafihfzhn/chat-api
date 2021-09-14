fd1b33daefbb9700455f6bce9ceb0ca5a5e5dae6
wafihfzhn <hafizhuddin96@gmail.com>
2021-09-14 16:33:23 +0800

    Install annotate

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: conversations
+#
+#  id          :bigint           not null, primary key
+#  sender_id   :bigint           not null
+#  receiver_id :bigint           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Conversation < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,27 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  provider               :string           default("email"), not null
+#  uid                    :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  allow_password_change  :boolean          default(FALSE)
+#  remember_created_at    :datetime
+#  confirmation_token     :string
+#  confirmed_at           :datetime
+#  confirmation_sent_at   :datetime
+#  unconfirmed_email      :string
+#  full_name              :string
+#  email                  :string
+#  tokens                 :json
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
