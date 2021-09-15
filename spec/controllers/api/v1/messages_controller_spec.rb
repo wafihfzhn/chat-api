eb4d39fa3011fde91d767c5ec4ad8d2cc8fb9ab1
wafihfzhn <hafizhuddin96@gmail.com>
2021-09-15 14:39:59 +0800

    Add scenarion test in messages controller spec

--- spec/controllers/api/v1/messages_controller_spec.rb
+++ spec/controllers/api/v1/messages_controller_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::MessagesController, type: :controller do
-  fixtures :users
+  fixtures :users, :conversations, :messages
 
   let(:user) { User.first }
 
   before do
     request.headers.merge! user.create_new_auth_token
+  end
+
+  it 'Create conversation if user never chat' do
+    post :create, params: { receiver_id: User.third, message: { content: 'Test send text' } }
+    expect(response.status).to eq(200)
+    expect(Conversation.count).to eq(3)
   end
 
   it 'Cannot send message to yourself' do
@@ -17,7 +23,6 @@ RSpec.describe Api::V1::MessagesController, type: :controller do
   it 'Successfully send message' do
     post :create, params: { receiver_id: User.second, message: { content: 'Test send text' } }
     expect(response.status).to eq(200)
-    expect(Message.count).to eq(1)
-    expect(Conversation.count).to eq(1)
+    expect(Message.count).to eq(4)
   end
 end
