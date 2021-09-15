7341fbbfcdbcbc06ce715d4a8bfb919ed19d072d
wafihfzhn <hafizhuddin96@gmail.com>
2021-09-15 11:29:42 +0800

    Add fixtures for test scenarios

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,7 @@ module ChatApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    ENV['FIXTURES_PATH'] = 'spec/fixtures'
   end
 end
