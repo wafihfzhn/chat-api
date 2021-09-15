2e6cd5fd29e5995ea86855d704c780dab0a6fc6d
wafihfzhn <hafizhuddin96@gmail.com>
2021-09-15 12:25:02 +0800

    Add testing list user's contacts

---
+++ spec/controllers/api/v1/contacts_controller_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::ContactsController, type: :controller do
+  fixtures :users
+
+  let(:user) { User.first }
+
+  before do
+    request.headers.merge! user.create_new_auth_token
+  end
+
+  it 'List contacts without current user' do
+    get :index
+    expect(JSON.parse(response.body)['data']).to eq(User.where.not(id: user).as_json)
+  end
+end
